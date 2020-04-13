06b7a795fa4026e7600f8d296cee76e5be0f9ff2
raghavrithvik <raghav.glass@gmail.com>
2020-04-13 19:09:31 +0530

    Update functions.sh

--- letsdnsocloud/functions.sh
+++ letsdnsocloud/functions.sh
@@ -43,6 +43,19 @@ function updateip() {
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
     -H "Content-Type: application/json"\
+    --data '{"type":"A","name":"'$LE_DOMAINS'","content":"'$1'","proxied":false}' -o /dev/null
+
+  echo "Updated $LE_DOMAINS with IP: $1"
+
+}
+
+#Update A record IP address
+function updateip2() {
+
+  curl -sX PUT "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$AID"\
+    -H "X-Auth-Email: $CF_EMAIL"\
+    -H "X-Auth-Key: $CF_APIKEY"\
+    -H "Content-Type: application/json"\
     --data '{"type":"A","name":"'$BASEDOMAIN'","content":"'$1'","proxied":false}' -o /dev/null
 
   echo "Updated $BASEDOMAIN with IP: $1"
