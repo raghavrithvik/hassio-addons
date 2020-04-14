8d2afd423967c5fd979bcccbe98f7721b930761e
raghavrithvik <raghav.glass@gmail.com>
2020-04-14 11:16:26 +0530

    Update functions.sh

--- letsdnsocloud/functions.sh
+++ letsdnsocloud/functions.sh
@@ -4,7 +4,7 @@
 function grabzoneid() {
 
   #Strip Subdomain to get bare domain
-  BASEDOMAIN=$(sed 's/.*\.\(.*\..*\)/\1/' <<< $LE_DOMAINS)
+  BASEDOMAIN=$LE_DOMAINS
 
   #Grab Zoneid & Export for Hooks.sh
   export ZONEID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones" \
@@ -20,16 +20,6 @@ function grabaid1() {
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
     -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$LE_DOMAINS'"))) | (select (.type | contains("A"))) | .id')
-
-}
-
-#Grab id 2 from existing A record
-function grabaid2() {
-
-  AID2=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records" \
-    -H "X-Auth-Email: $CF_EMAIL"\
-    -H "X-Auth-Key: $CF_APIKEY"\
-    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | (select (.type | contains("A"))) | .id')
 
 }
 
@@ -56,13 +46,5 @@ function updateip() {
     --data '{"type":"A","name":"'$LE_DOMAINS'","content":"'$1'","proxied":false}' -o /dev/null
 
   echo "Updated $LE_DOMAINS with IP: $1"
-
-  curl -sX PUT "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$AID2"\
-    -H "X-Auth-Email: $CF_EMAIL"\
-    -H "X-Auth-Key: $CF_APIKEY"\
-    -H "Content-Type: application/json"\
-    --data '{"type":"A","name":"'$BASEDOMAIN'","content":"'$1'","proxied":false}' -o /dev/null
-
-  echo "Updated $BASEDOMAIN with IP: $1"
 
 }
