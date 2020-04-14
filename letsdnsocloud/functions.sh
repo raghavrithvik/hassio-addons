dc37235f2b597504dcfecb1deee1e96bbff92b91
raghavrithvik <raghav.glass@gmail.com>
2020-04-14 11:33:16 +0530

    Update functions.sh

--- letsdnsocloud/functions.sh
+++ letsdnsocloud/functions.sh
@@ -10,7 +10,8 @@ function grabzoneid() {
   export ZONEID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones" \
     -H "X-Auth-Email: $CF_EMAIL" \
     -H "X-Auth-Key: $CF_APIKEY" \
-    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | .id')
+    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | .id' \
+    echo "step 1 done")
 }
 
 #Grab id 1 from existing A record
@@ -19,7 +20,8 @@ function grabaid() {
   AID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records" \
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
-    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | (select (.type | contains("A"))) | .id')
+    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | (select (.type | contains("A"))) | .id' \
+    echo "step 2 done")
 
 }
 
